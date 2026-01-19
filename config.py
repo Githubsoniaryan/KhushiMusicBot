3376d7ade6afb6d4aeb33453001f5e2015ab7d50
SoniAryanXmart <154493242+Githubsoniaryan@users.noreply.github.com>
2026-01-19 20:20:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -54,8 +54,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/tabahi_tabahi")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/tabahi_tabahi")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/sahityaprerna")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/sayriyann")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -182,6 +182,7 @@ if SUPPORT_GROUP:
         raise SystemExit(
             "[ERROR] - Your SUPPORT_GROUP url is wrong. Please ensure that it starts with https://"
         )
+
 
 
 
