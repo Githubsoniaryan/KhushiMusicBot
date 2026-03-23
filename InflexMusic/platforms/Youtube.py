23d8b57b2159433a9e590eadf7a53b07a804fcdf
SoniAryanXmart <154493242+Githubsoniaryan@users.noreply.github.com>
2026-03-23 16:33:26 +0530

    Update Youtube.py

--- InflexMusic/platforms/Youtube.py
+++ InflexMusic/platforms/Youtube.py
@@ -6,9 +6,9 @@ import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from py_yt import VideosSearch, Playlist
-from BrandrdXMusic.utils.formatters import time_to_seconds
+from InflexMusic.utils.formatters import time_to_seconds
 import aiohttp
-from BrandrdXMusic import LOGGER
+from InflexMusic import LOGGER
 
 API_URL = "https://shrutibots.site"
 DOWNLOAD_DIR = "downloads"
